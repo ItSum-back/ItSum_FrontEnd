d38f75d36d4a69fd9c593381ffca662248859cae
similar207 <schk5089@naver.com>
2023-04-04 20:07:59 +0900

    commit

---
+++ app/src/main/java/com/example/itsum/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.itsum
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+
+}
