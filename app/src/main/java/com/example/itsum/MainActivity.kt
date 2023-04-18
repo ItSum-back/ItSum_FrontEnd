befde82813e947a5843329cbfd7f206d7f68ee69
similar207 <schk5089@naver.com>
2023-04-18 15:48:04 +0900

    2023.04.18_Ryan

--- app/src/main/java/com/example/itsum/MainActivity.kt
+++ app/src/main/java/com/example/itsum/MainActivity.kt
@@ -3,30 +3,37 @@ package com.example.itsum
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
+import com.kakao.sdk.common.util.Utility
 import android.widget.Button
+import com.example.itsum.retrofit.RetrofitConnection
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //해시키 구하기
+        //val keyHash = Utility.getKeyHash(this)
+        //Log.d("Hash", keyHash)
 
-        login_btn.setOnClickListener {
+        login_btn.setOnClickListener {  //로그인버튼 눌렀을 때
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
-        join_btn.setOnClickListener {
+        join_btn.setOnClickListener {   //회원가입 눌렀을 때
             val intent = Intent(this, NewJoin::class.java)
             startActivity(intent)
+        }
+        kakao_login.setOnClickListener {
+            val retro = RetrofitConnection.getInstance()
         }
 
 
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
-
-        //supportActionBar?.setDisplayHomeAsUpEnabled(true)
         return true
     }
 
